ce418c29ab960c113b25584d0c0a75931195e549
PhotoNomad0 <bruce.mclean@unfoldingword.org>
2023-09-25 08:37:00 -0400

    Added customization of ltr style

--- src/lib/previewStyling.js
+++ src/lib/previewStyling.js
@@ -6,15 +6,13 @@ import {
 } from '@oce-editor-tools/core'
 
 export function getRtlPreviewStyle() {
-  const _renderStyles = cloneDeep(renderStylesRtl)
+  const _renderStyles = cloneDeep(renderStylesRtl) // make clone to allow for modification without breaking original
   _renderStyles.marks.chapter_label.textIndent = 0
-  _renderStyles.paras['usfm:mt'].textAlign = 'right'
+  _renderStyles.paras['usfm:mt'].textAlign = 'right' // move title to right
   return _renderStyles
 }
 
 export function getLtrPreviewStyle() {
-  const _renderStyles = {
-    ...renderStyles,
-  }
+  const _renderStyles = cloneDeep(renderStyles) // make clone to allow for modification without breaking original
   return _renderStyles
 }
